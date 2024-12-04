48d1bf4ef3c0b32b07ac82501a6d7737779d0028
Theo VanderMarck <theo.vandermarck@outlook.com>
2024-12-04 09:37:48 -0800

    idk

--- htmlreading.py
+++ htmlreading.py
@@ -7,33 +7,33 @@ words = {}
 cullThreshold = 1
 with open(filePath, 'r+') as myFile:
     content = myFile.read()
+    # print(content)
     while True:
         while True:
-            if content.find('</span>')<content.find('<span class="bubble">'):
+            if content.find('</span>\n</div>')<content.find('<span class="sender">Me'):
                 print('r' + str(len(content)))
-                # content[lastIndex:content.find('</span>')]=''
-                content = content[:lastIndex]+content[content.find('</span>')+7:]
+                content = content[:lastIndex]+content[content.find('</span>\n</div>')+14:]
             else:
                 break
     
         # print(str(content.find('<span class="bubble">')) +" "+ str(content.find("</span>")))
-        print(content[content.find('<span class="bubble">')+21:content.find('</span>')])
-        tbad = (content[content.find('<span class="bubble">')+21:content.find('</span>')])
+        tbad = (content[content.find('<span class="sender">Me')+87:content.find('</span>\n</div>')])
+        print(tbad)
         for a in replaceList:
             tbad = tbad.replace(a[0], a[1])
         for a in removeList:
             tbad = tbad.replace(a, '')
-        print(tbad)
         texts.append(tbad)
-        lastIndex = content.find('</span>')+7
+        lastIndex = content.find('</span>\n</div>')+14
         content = content[lastIndex:]
         lastIndex = 0
-        if content.find('<span class="bubble">')==-1:
+        if content.find('<span class="sender">Me')==-1:
             break
-# for i in range(len(texts)):
-#     for a in removeList:
-#         texts[i] = texts[i].replace(a, '')
-#         print("e")
+            
+for i in range(len(texts)):
+    for a in removeList:
+        texts[i] = texts[i].replace(a, '')
+        print("e")
 for text in texts:
     for word in text.split():
         if word.lower() in words:
